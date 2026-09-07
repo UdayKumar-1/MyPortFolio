e07a8b40746871ab486fce5e872e404bec84d934
Uday Kumar <annaldasudaykumar1@gmail.com>
2026-09-07 22:55:24 +0530

    added comment.

--- MyPortFolio/PortFolio.js
+++ MyPortFolio/PortFolio.js
@@ -6,6 +6,7 @@ for(var i=0;i<arrow.length;i++){
     arrowParent.classList.toggle("showMenu");
   })
 }
+//added comment.
 //to close the sidebar when i clicked the menu button.
 let sidebar=document.querySelector(".sidebar");
 let sidebarBtn=document.querySelector(".bx-menu");
